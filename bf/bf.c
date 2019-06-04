4734fae3d80835b27c752a569d06b6c1c30aa730
emanuel <emanuelzetka@gmail.com>
2019-06-04 15:02:40 -0300

    solucionar problema

--- bf/bf.c
+++ bf/bf.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
 void imprimir_br(int vector[]){
 	char br_vector[300000];
@@ -11,35 +12,75 @@ void imprimir_br(int vector[]){
 	int corchetes[500];//vector donde se guarda la pos de los [
 	int pos_corchete = 0;
 
-	//int w = 0;
+	int w = 0;
 	for(int i = 0; vector[i] != '\n'; i++){
-		//printf("%c\n", vector[i]);
-		//printf("i = %d\n", i);
+		printf("%c || %d\n", vector[i],i);
 		if(vector[i] == '.')putchar(br_vector[pos]);
 		if(vector[i] == '>') pos++;
 		if(vector[i] == '<') pos--;
 		if(vector[i] == '+') br_vector[pos]++;
 		if(vector[i] == '-') br_vector[pos]--;
 		if(vector[i] == '['){
+			if(br_vector[pos] == 0){
+				printf("entro aca\n");
+				while(vector[i] != ']'){
+					i++;
+					//aca esta el problema
+				}
+				continue;
+			}
 			//guardo la pos del corchete inicial
 			//printf("por i [ = %d\n", i );
 			corchetes[pos_corchete] = i;
-			//printf("aca esta guardado el corchete = %d||%d\n", corchetes[pos_corchete],pos_corchete);
+			printf("-----------guardado el corchete = %d||%d\n", corchetes[pos_corchete],pos_corchete);
 			pos_corchete++;
 		}
 		if(vector[i] == ']'){
 			//si br_vector se encuentra en 0 no vuelve al inicio del [
 			if(br_vector[pos] == 0){
+				printf("entro ac2\n");
+				corchetes[pos_corchete] = 0;
 				pos_corchete--;
 				continue;
 			}
 			i = corchetes[pos_corchete - 1];
 		}
-		//if(w == 70)return;
-		//w++;
-		//printf("br = %d||%d\n",br_vector[pos], pos);
+		if(w == 800)return;
+		w++;
+		
+		printf("[");
+		for(int a = 0; a < 10; a++){
+			printf("%d ", br_vector[a]);
+			if(a == pos){
+				putchar(35);
+				printf("(%d)",pos);
+			}
+			printf(",");
+		}
+		printf("]\n");
 
+		printf("[");
+		for(int b = 0; b < 5; b++){
+			printf("%d", corchetes[b]);
+			if(b == pos_corchete)putchar(35);
+			printf(",");
+		}
+		printf("]\n");
+
+		printf("*******************************************************************\n");
+		
 	}
+}
+bool validos(int caracter){
+	if(caracter == '.') return true;
+	if(caracter == '>') return true;
+	if(caracter == '<') return true;
+	if(caracter == '+') return true;
+	if(caracter == '-') return true;
+	if(caracter == '[') return true;
+	if(caracter == ']') return true;
+	if(caracter == ',') return true;
+	return false;
 }
 
 void leer_lineas_br(FILE* archivo){
@@ -47,8 +88,10 @@ void leer_lineas_br(FILE* archivo){
 	int char_leido;
 	int i = 0;
 	while((char_leido = fgetc(archivo)) != EOF){
-		vector_br[i] = char_leido;
-		i++;
+		if(validos(char_leido)){
+			vector_br[i] = char_leido;
+			i++;
+		}
 	}
 	vector_br[i] = '\n';
 	fclose(archivo);
