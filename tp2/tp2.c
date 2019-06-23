7cfe6d5ac18538199227a1b233d7e8cae7f65a70
emanuel <emanuelzetka@gmail.com>
2019-06-23 19:57:08 -0300

    empezando a ordenar

--- tp2/tp2.c
+++ tp2/tp2.c
@@ -4,6 +4,12 @@
 #include <string.h>
 #include "strutil.h"
 #include "hash.h"
+
+void couting_sort_simple(const char* clave, size_t cant){
+
+}
+
+
 void cant_tuit_por_usuario(const char* linea, hash_t* hash){
 	char** vector = split(linea,',');
 	hash_t* hash_tuit = hash_obtener(hash, vector[0]); //devulve un hash o NULL
@@ -24,8 +30,16 @@ void leer_archivo(FILE* archivo){
 	while((getline( &linea, &bytes_leidos, archivo)) > 0){
 		cant_tuit_por_usuario(linea,hash_usuario);
 	};
-	
 	free(linea);
+
+	hash_iter_t* iter = hash_iter_crear(hash);
+	while(!hash_iter_al_final(iter)){
+		const char* clave = hash_iter_ver_actual(iter);
+		hash_t* valor = hash_obtener(hash,clave);
+
+		size_t cant = hash_cantidad(valor);
+		couting_sort_simple(clave,cant);
+	} 
 
 }
 
