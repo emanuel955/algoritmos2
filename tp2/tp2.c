027415b545f8ff07d4e955e1d840c8fc55d614b2
emanuel <emanuelzetka@gmail.com>
2019-06-25 14:16:09 -0300

    falta ordenarpor cantidad y nombres

--- tp2/tp2.c
+++ tp2/tp2.c
@@ -5,25 +5,44 @@
 #include "strutil.h"
 #include "hash.h"
 
-void couting_sort_simple(const char* clave, size_t cant){
+void couting_sort_simple(hash_t* hash){
+	hash_iter_t* iter = hash_iter_crear(hash);
 
+	while(!hash_iter_al_final(iter)){
+		const char* clave = hash_iter_ver_actual(iter);
+		hash_t* valor = hash_obtener(hash,clave);
+
+		size_t cant = hash_cantidad(valor);
+		printf("%s||%ld\n", clave,cant );
+		hash_iter_avanzar(iter);
+	}
+	hash_iter_destruir(iter);
 }
-
-
 void cant_tuit_por_usuario(const char* linea, hash_t* hash){
 	char** vector = split(linea,',');
+	if(strlen(vector[0]) > 15)return;
+	//printf("FUNCION usuario = %s\n", vector[0]);
 	hash_t* hash_tuit = hash_obtener(hash, vector[0]); //devulve un hash o NULL
 	if(!hash_tuit){
+		//printf("ENTRO EN EL DATO NO ES HASH\n");
 		hash_tuit = hash_crear(free);//si no esta el usuario lo guarda junto con un hash
 		hash_guardar(hash,vector[0],hash_tuit);
 	}
+	free(vector[0]);
 	for(size_t i = 1; vector[i] != NULL; i++){
+		//printf("------- %s\n", vector[i]);
+		char* tag = vector[]//sacar en /n
 		hash_guardar(hash_tuit,vector[i],NULL);
+		free(vector[i]);
 	}
+	free(vector);
+	//printf("\n");
 }
-
+void destruir_dato(void* dato){
+	hash_destruir(dato);
+}
 void leer_archivo(FILE* archivo){
-	hash_t* hash_usuario = hash_crear(free);
+	hash_t* hash_usuario = hash_crear(destruir_dato);
 
 	char* linea = NULL;
 	size_t bytes_leidos;
@@ -31,18 +50,10 @@ void leer_archivo(FILE* archivo){
 		cant_tuit_por_usuario(linea,hash_usuario);
 	};
 	free(linea);
+	couting_sort_simple(hash_usuario);
 
-	hash_iter_t* iter = hash_iter_crear(hash);
-	while(!hash_iter_al_final(iter)){
-		const char* clave = hash_iter_ver_actual(iter);
-		hash_t* valor = hash_obtener(hash,clave);
-
-		size_t cant = hash_cantidad(valor);
-		couting_sort_simple(clave,cant);
-	} 
-
+	hash_destruir(hash_usuario);
 }
-
 int main(int argc, char const *argv[]){
 	FILE* archivo = fopen(argv[1], "r");
 		if( archivo == NULL ){
@@ -50,5 +61,6 @@ int main(int argc, char const *argv[]){
 			return 1;
 		} 
 		leer_archivo(archivo);
+		fclose(archivo);
 	return 0;
 }
